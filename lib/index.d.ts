816a4f862f6f6368b9198a68d38d2c73b749f90e
Prokop Schield <prokop.schield@protonmail.com>
2021-03-24 16:42:51 +0100

    0.0.4 - nslibmgr

---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+import { format, UnformattedMessageChunk, UnformattedMessageChunkLiteral, UnformattedMessageChunkObject, FormattedMessageChunk } from './format';
+import { messageToString } from './messageToString';
+export { format, UnformattedMessageChunk, UnformattedMessageChunkLiteral, UnformattedMessageChunkObject, FormattedMessageChunk, messageToString, };
---
+++ lib/messageToString.d.ts
@@ -0,0 +1,4 @@
+import { FormattedMessageChunk } from './format';
+export declare function messageToString(tokens: FormattedMessageChunk[], { plain, }?: {
+    plain: boolean;
+}): string;
